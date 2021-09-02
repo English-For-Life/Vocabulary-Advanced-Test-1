06c70c633005b135edf8343d1b1474d9f807d7b5
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-09-02 13:34:41 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"Sponsorship is being increasingly allowed in schools provided it is regarded as being ------- with educational curricula.",
+        options:["compatible", "provocative", "decisive", "predictable", "considerate"],
+        answer: 0
+    },
+    {
+        q:"As the new field worker will be working in close ------- with several others, make sure you select someone with agreeable personality.",
+        options:["suspicion", "collaboration", "uniformity", "discretion", "productivity"],
+        answer: 1
+    },
+    {
+        q:"The discovery of a ------- cancer-causing chemical in foods like crisps, chips and cereals caused shockwaves around the world when it hit the headlines earlier this year.",
+        options:["constantly", "completely", "potentially", "radically", "remarkably"],
+        answer: 2
+    },
+    {
+        q:"The study of the situation covers a great deal of relevant material, but does not tackle the real issues ------ enough.",
+        options:["currently", "vaguely", "outrageously", "adequately", "disturbingly"],
+        answer: 3
+    },
+    {
+        q:"The recent recovery in palm oil production in Southeast Asia has lead to a / an ----- decline in all vegetable oil prices.",
+        options:["dense", "tentative", "clumsy", "abstract", "dramatic"],
+        answer: 4
+    },
+    {
+        q:"At present, the rate of economic growth is very satisfactory, but ------ when foreign competition makes itself felt, this will not be the case.",
+        options:["eventually", "enthusiastically", "considerably", "effectively", "preferably"],
+        answer: 0
+    },
+    {
+        q:"Serbian ------ in Bosnia has been strongly condemned by the civilised world, but little serious action has been taken against it.",
+        options:["settlement", "conduct", "investment", "treaty", "compliment"],
+        answer: 1
+    },
+    {
+        q:"In view of the severe economic recession, his appointment to this office was regarded as highly ------ .",
+        options:["considerable", "compulsory", "appropriate", "persuasive", "refutable"],
+        answer: 2
+    },
+    {
+        q:"In a child, curiosity normally suggests intelligence and is welcomed, but an ------ adult is best avoided.",
+        options:["indefinite", "indulgent", "indecisive", "inquisitive", "impartial"],
+        answer: 3
+    },
+    {
+        q:"I am not ------ that the proposal actually will lead to an improvement in the situation.",
+        options:["impressed", "deterred", "refrained", "intrusive", "convinced"],
+        answer: 4
+    }
+
+
+
+]
